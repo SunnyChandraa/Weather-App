816ef1b62abe70fc0442032d75acd215ccfe265b
Sunnychandr <sunny.chandra310505@gmail.com>
2022-09-27 10:43:42 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Observable, Subject } from 'rxjs';
+import { map, takeUntil } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-  title = 'weather-app';
+export class AppComponent implements OnInit, OnDestroy {
+  private unsubscribe$ = new Subject<void>();
+  countries = [
+    {
+      name: "Indonesia",
+      cities: ["Jakarta", "Bandung", "Tangerang"]
+    },
+    {
+      name: "United States",
+      cities: ["New York", "Chicago", "Washington"]
+    },
+    {
+      name: "Germany",
+      cities: ["Berlin", "Hamburg", "Melbourne"]
+    },
+    {
+      name: "Pakistan",
+      cities: ["Lahore", "Karachi", "Islamabad"]
+    }
+  ];
+
+  countryControl: FormControl;
+  cityControl: FormControl;
+  cities$: Observable<String>;
+
+  constructor(private router: Router) {
+    this.cityControl = new FormControl('');
+    this.cityControl.valueChanges
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(value => {
+        this.router.navigate([value]);
+      });
+
+    this.countryControl = new FormControl('');
+
+    this.cities$ = this.countryControl.valueChanges.pipe(
+      map(country => country.cities)
+    );
+  }
+
+  ngOnInit() {
+    
+  }
+
+  ngOnDestroy() {
+  }
+
+  
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { WeatherReportComponent } from './weather-report/weather-report.component';
+import { HttpClientModule } from '@angular/common/http';
 
+const routes: Routes = [
+  {
+    path: "",
+    component: WeatherReportComponent
+  },
+  {
+    path: ":locationName",
+    component: WeatherReportComponent
+  }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherReportComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NoopAnimationsModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+
+
 export class AppModule { }
